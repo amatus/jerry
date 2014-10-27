744ca80687aea3a9f0160a87d26f175a1c28a288
David Barksdale <amatus@amatus.name>
2014-10-27 09:49:10 -0500

    New script for users of the GoBook XR-1

---
+++ gobook-xr1.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Enable touchscreen
+sudo install -m 755 gobook-xr1-touchscreen /etc/init.d/
+sudo update-rc.d gobook-xr1-touchscreen defaults
+
+# Calibrate touchscreen
+sudo apt-get install -y xinput-calibrator
+xinput_calibrator | sed -n -e '/Section/,/EndSection/p' > 99-calibration.conf
+sudo install -m 644 99-calibration.conf /usr/share/X11/xorg.conf.d/
+rm 99-calibration.conf
