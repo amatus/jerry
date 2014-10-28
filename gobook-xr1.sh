d04fbcf4b89dfd56f03dd0824e4774ca7739f3ac
David Barksdale <amatus@amatus.name>
2014-10-28 12:54:44 -0500

    Touchscreen fixes

--- gobook-xr1.sh
+++ gobook-xr1.sh
@@ -2,13 +2,15 @@
 
 # Enable touchscreen
 sudo install -m 755 gobook-xr1-touchscreen /etc/init.d/
+sudo /etc/init.d/gobook-xr1-touchscreen start
 sudo update-rc.d gobook-xr1-touchscreen defaults
 
 # Calibrate touchscreen
 if ! test -e /usr/share/X11/xorg.conf.d/99-calibration.conf; then
   sudo apt-get install -y xinput-calibrator
   xinput_calibrator | sed -n -e '/Section/,/EndSection/p' > 99-calibration.conf
-  sudo install -m 644 99-calibration.conf /usr/share/X11/xorg.conf.d/
+  test -s 99-calibration.conf &&
+    sudo install -m 644 99-calibration.conf /usr/share/X11/xorg.conf.d/
   rm 99-calibration.conf
 fi
 
